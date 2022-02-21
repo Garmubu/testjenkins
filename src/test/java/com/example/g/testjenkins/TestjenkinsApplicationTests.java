3eed8914451b5933700adb7a5cc228d26ee950eb
Billel <billel.emerton@gmail.com>
2022-02-21 14:27:39 +0100

    sheesh

--- src/test/java/com/example/g/testjenkins/TestjenkinsApplicationTests.java
+++ src/test/java/com/example/g/testjenkins/TestjenkinsApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.g.testjenkins;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TestjenkinsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(5,4);
 	}
 
 }
